886599f33adbefd317f9ae0e87ea0a766ccfa6b2
Hamza <iklafen.hamza@gmail.com>
2023-08-29 14:53:20 -0700

    updates

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
     size_t num = 0;
 
     while (h)
     {
-        printf("%d\n", h->n);
         num++;
         h = h->next;
     }
