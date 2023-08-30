2257e0ebc4d13e5acb9bb1ecb203d00d77392e6f
Hamza <iklafen.hamza@gmail.com>
2023-08-29 17:59:25 -0700

    all files updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,3 @@
-lists.h
-
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -16,8 +14,8 @@ lists.h
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+ int n;
+ struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
